476d8476f67dc6f65494e4624df437f9fb5628bc
Andrew <unenergizer@gmail.com>
2019-10-28 20:34:54 -0500

    Game map updates

--- src/main/java/com/valenguard/server/game/PlayerConstants.java
+++ src/main/java/com/valenguard/server/game/PlayerConstants.java
@@ -23,12 +23,12 @@ public class PlayerConstants {
     /**
      * First player join spawn location.
      */
-    public static final Location START_SPAWN_LOCATION = new Location("game_start", (short) 39, (short) 5);
+    public static final Location START_SPAWN_LOCATION = new Location("game_start", (short) 39, (short) 48);
 
     /**
      * Player death spawn location.
      */
-    public static final Location RESPAWN_LOCATION = new Location("game_start", (short) 39, (short) 5);
+    public static final Location RESPAWN_LOCATION = new Location("game_start", (short) 39, (short) 48);
 
     /**
      * Player Starting health points
