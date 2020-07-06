1f0034852204c388f1c1f4cf74a37e94151074f9
unenergizer <unenergizer@gmail.com>
2020-07-05 19:54:35 -0500

    Revert ChatMessagePacketOut.java

--- src/main/java/com/forgestorm/server/network/game/packet/out/ChatMessagePacketOut.java
+++ src/main/java/com/forgestorm/server/network/game/packet/out/ChatMessagePacketOut.java
@@ -1,37 +1,28 @@
 package com.forgestorm.server.network.game.packet.out;
 
-import com.forgestorm.server.database.AuthenticatedUser;
 import com.forgestorm.server.game.ChatChannelType;
 import com.forgestorm.server.game.world.entity.Player;
 import com.forgestorm.server.network.game.shared.Opcodes;
-
-import static com.forgestorm.server.util.Log.println;
+import com.forgestorm.server.util.Log;
 
 public class ChatMessagePacketOut extends AbstractServerOutPacket {
 
     private static final boolean PRINT_DEBUG = false;
 
-    private final Player player;
     private final ChatChannelType chatChannelType;
     private final String message;
 
     public ChatMessagePacketOut(final Player player, final ChatChannelType chatChannelType, final String message) {
         super(Opcodes.CHAT, player.getClientHandler());
-        this.player = player;
         this.chatChannelType = chatChannelType;
         this.message = message;
     }
 
     @Override
     protected void createPacket(GameOutputStream write) {
-        // Don't send staff chat messages to clients that shouldn't read them
-        if (chatChannelType == ChatChannelType.STAFF) {
-            AuthenticatedUser authenticatedUser = player.getClientHandler().getAuthenticatedUser();
-            if (!authenticatedUser.isAdmin() || !authenticatedUser.isModerator()) return;
-        }
         write.writeByte(ChatChannelType.getByte(chatChannelType));
         write.writeString(message);
-        println(getClass(), chatChannelType.name(), false, PRINT_DEBUG);
-        println(getClass(), message, false, PRINT_DEBUG);
+        Log.println(getClass(), chatChannelType.name(), false, PRINT_DEBUG);
+        Log.println(getClass(), message, false, PRINT_DEBUG);
     }
 }
