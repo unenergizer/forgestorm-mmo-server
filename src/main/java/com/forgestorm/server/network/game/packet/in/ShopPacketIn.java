3c133b03262e7edfd753ebab7699cba712dd5f8c
unenergizer <unenergizer@gmail.com>
2020-08-03 13:18:45 -0500

    Fix for non-working NPC shops

--- src/main/java/com/forgestorm/server/network/game/packet/in/ShopPacketIn.java
+++ src/main/java/com/forgestorm/server/network/game/packet/in/ShopPacketIn.java
@@ -10,20 +10,28 @@ import lombok.AllArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.forgestorm.server.util.Log.println;
+
 @Opcode(getOpcode = Opcodes.ENTITY_SHOPS)
 public class ShopPacketIn implements PacketListener<ShopPacketIn.ShopPacket>, PacketInCancelable {
+
+    private static final boolean PRINT_DEBUG = false;
 
     @Override
     public PacketData decodePacket(ClientHandler clientHandler) {
         ShopOpcodes npcShopOpcode = ShopOpcodes.getShopOpcode(clientHandler.readByte());
-        short entityId = 0;
-        short shopSlot = 0;
+        short entityId = -1;
+        short shopSlot = -1;
 
         if (npcShopOpcode == ShopOpcodes.START_SHOPPING) {
             entityId = clientHandler.readShort();
         } else if (npcShopOpcode == ShopOpcodes.BUY) {
             shopSlot = clientHandler.readShort();
         }
+
+        println(getClass(), "Opcode: " + npcShopOpcode, false, PRINT_DEBUG);
+        println(getClass(), "EntityId: " + entityId, false, PRINT_DEBUG);
+        println(getClass(), "ShopSlot: " + shopSlot, false, PRINT_DEBUG);
 
         return new ShopPacket(npcShopOpcode, entityId, shopSlot);
     }
@@ -35,21 +43,30 @@ public class ShopPacketIn implements PacketListener<ShopPacketIn.ShopPacket>, Pa
         // The player cannot move and shop at the same time.
         if (player.isEntityMoving()) {
             player.setCurrentShoppingEntity(null);
+            println(getClass(), "Player moved, chancel shopping.", false, PRINT_DEBUG);
             return false;
         }
 
         if (packetData.shopOpcode == ShopOpcodes.START_SHOPPING) {
-            if (player.getGameMap().getAiEntityController().doesNotContainKey(packetData.entityId))
+            if (player.getGameMap().getAiEntityController().doesNotContainKey(packetData.entityId)) {
+                println(getClass(), "Entity does not exist, can not start shopping.", false, PRINT_DEBUG);
                 return false;
+            }
         }
 
         if (packetData.shopOpcode == ShopOpcodes.START_SHOPPING) {
             AiEntity aiEntity = (AiEntity) player.getGameMap().getAiEntityController().getEntity(packetData.entityId);
-            if (aiEntity.getShopId() == -1) return false;
+            if (aiEntity.getShopId() == -1) {
+                println(getClass(), "Shop value is -1 (shop does not exist for this entity)", false, PRINT_DEBUG);
+                return false;
+            }
         }
 
         if (packetData.shopOpcode == ShopOpcodes.BUY || packetData.shopOpcode == ShopOpcodes.SELL) {
-            return player.getCurrentShoppingEntity() != null;
+            if (player.getCurrentShoppingEntity() == null) {
+                println(getClass(), "Player has not chosen an entity to shop from.", false, PRINT_DEBUG);
+                return false;
+            }
         }
 
         return true;
@@ -61,11 +78,14 @@ public class ShopPacketIn implements PacketListener<ShopPacketIn.ShopPacket>, Pa
         Player player = packetData.getClientHandler().getPlayer();
 
         if (packetData.shopOpcode == ShopOpcodes.START_SHOPPING) {
+            println(getClass(), "Setting player shop entity.", false, PRINT_DEBUG);
             player.setCurrentShoppingEntity((AiEntity) player.getGameMap().getAiEntityController().getEntity(packetData.entityId));
         } else if (packetData.shopOpcode == ShopOpcodes.BUY) {
+            println(getClass(), "Player is buying an item.", false, PRINT_DEBUG);
             ServerMain.getInstance().getEntityShopManager()
                     .playerBuyItemStack(player.getCurrentShoppingEntity().getShopId(), packetData.shopSlot, player);
         } else if (packetData.shopOpcode == ShopOpcodes.STOP_SHOPPING) {
+            println(getClass(), "Player is no longer shopping.", false, PRINT_DEBUG);
             player.setCurrentShoppingEntity(null);
         }
     }
